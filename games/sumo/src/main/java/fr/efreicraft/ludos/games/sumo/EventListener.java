4d64a5df33fd70f49215108a2925c2d632b61888
DocSystem <42613513+DocSystem@users.noreply.github.com>
2022-11-19 19:40:52 +0100

    Add Game Events (on move, on damage)

---
+++ games/sumo/src/main/java/fr/efreicraft/ludos/games/sumo/EventListener.java
@@ -0,0 +1,36 @@
+package fr.efreicraft.ludos.games.sumo;
+
+import fr.efreicraft.ludos.core.Core;
+import fr.efreicraft.ludos.core.players.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public record EventListener(GameLogic sumo) implements Listener {
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (!event.hasChangedBlock()) {
+            return;
+        }
+        Player player = Core.get().getPlayerManager().getPlayer(event.getPlayer());
+        if (!player.getTeam().isPlayingTeam()) {
+            return;
+        }
+        if (this.sumo.isOutsideKillzone(event.getTo().getY())) {
+            sumo.onPlayerBelowKillzone(player);
+        }
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof org.bukkit.entity.Player) {
+            Player player = Core.get().getPlayerManager().getPlayer((org.bukkit.entity.Player) event.getEntity());
+            if (!player.getTeam().isPlayingTeam()) {
+                return;
+            }
+            player.entity().setHealth(20);
+        }
+    }
+}
