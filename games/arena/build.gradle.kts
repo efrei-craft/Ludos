12a02a2591630c997683f892b3fcc6437b78d15a
JiveOff <antoine@jiveoff.fr>
2022-11-19 05:45:45 +0100

    fix: add gradle build for arena

---
+++ games/arena/build.gradle.kts
@@ -0,0 +1,22 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
+plugins {
+    `java-library`
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2" // Generates plugin.yml
+}
+
+dependencies {
+    implementation(project(":core"))
+
+    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+}
+
+bukkit {
+    load = BukkitPluginDescription.PluginLoadOrder.POSTWORLD
+    name = "LudosArena"
+    main = "fr.efreicraft.ludos.games.arena.Main"
+    apiVersion = "1.19"
+    authors = listOf("Antoine BANHA", "Logan TANN")
+    prefix = "ARENA"
+    depend = listOf("LudosCore")
+}
