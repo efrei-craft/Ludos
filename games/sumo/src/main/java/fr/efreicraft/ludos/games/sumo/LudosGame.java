3b1b9baf40f3e352f2a3f5ce4c9b2abfd2989c23
DocSystem <42613513+DocSystem@users.noreply.github.com>
2022-11-19 19:42:19 +0100

    Create some teams, register kill zone (game point and event) and put minPlayers to 2

--- games/sumo/src/main/java/fr/efreicraft/ludos/games/sumo/LudosGame.java
+++ games/sumo/src/main/java/fr/efreicraft/ludos/games/sumo/LudosGame.java
@@ -1,15 +1,19 @@
 package fr.efreicraft.ludos.games.sumo;
 
+import fr.efreicraft.ludos.core.Core;
 import fr.efreicraft.ludos.core.games.interfaces.Game;
 import fr.efreicraft.ludos.core.games.interfaces.GameMetadata;
 import fr.efreicraft.ludos.core.players.Player;
 import fr.efreicraft.ludos.core.teams.DefaultTeamRecordBuilder;
 import fr.efreicraft.ludos.core.teams.TeamRecord;
+import fr.efreicraft.ludos.core.utils.ColorUtils;
 import org.bukkit.GameRule;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 
 import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.Map;
 
 @GameMetadata(
@@ -18,9 +22,18 @@ import java.util.Map;
         description = "Poussez votre adversaire en dehors du ring !",
         color = "&c",
         version = "1.0",
-        minPlayers = 1
+        minPlayers = 2
 )
 public class LudosGame extends Game {
+
+    private final GameLogic gameLogic;
+
+    public LudosGame() {
+        super();
+        this.gameLogic = new GameLogic();
+        this.setEventListener(new EventListener(this.gameLogic));
+    }
+
     @Override
     public void preMapParse(World world) {
         world.setGameRule(GameRule.DO_WEATHER_CYCLE, false);
@@ -32,7 +45,13 @@ public class LudosGame extends Game {
 
     @Override
     public void postMapParse() {
-
+        Location killZoneLocation = Core.get().getMapManager().getCurrentMap().getGamePoints()
+                .get("KILL_ZONE").get(0).getLocation();
+        this.gameLogic.setKillZoneLocation(killZoneLocation);
+        Core.get().getMapManager().getCurrentMap().getWorld().setBlockData(
+                killZoneLocation,
+                Material.AIR.createBlockData()
+        );
     }
 
     @Override
@@ -42,11 +61,31 @@ public class LudosGame extends Game {
 
     @Override
     public EnumMap<Material, String> getGamePointsMaterials() {
-        return null;
+        EnumMap<Material, String> gamePointsMaterials = new EnumMap<>(Material.class);
+        gamePointsMaterials.put(Material.BEDROCK, "KILL_ZONE");
+        return gamePointsMaterials;
     }
 
     @Override
     public Map<String, TeamRecord> getTeamRecords() {
-        return DefaultTeamRecordBuilder.DefaultTeamRecords.DEFAULT_TEAMS_SOLO.getTeamRecords();
+        HashMap<String, TeamRecord> teams = new HashMap<>();
+        teams.put("ROUGE", new TeamRecord(
+                "Rouge",
+                1,
+                true,
+                true,
+                new ColorUtils.TeamColorSet(ColorUtils.TeamColors.RED),
+                null
+        ));
+        teams.put("BLEU", new TeamRecord(
+                "Bleu",
+                2,
+                true,
+                true,
+                new ColorUtils.TeamColorSet(ColorUtils.TeamColors.BLUE),
+                null
+        ));
+        teams.putAll(DefaultTeamRecordBuilder.DefaultTeamRecords.ONLY_SPECTATOR.getTeamRecords());
+        return teams;
     }
 }
