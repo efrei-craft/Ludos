2c15c4fe18cd04a03c20e23450170128d70c14c7
Niilyx _ <nait.meddour.idir100@gmail.com>
2022-11-19 13:28:34 +0100

    init

---
+++ games/rush/src/main/java/fr/efreicraft/ludos/games/rush/LudosGame.java
@@ -0,0 +1,37 @@
+package fr.efreicraft.ludos.games.rush;
+
+import fr.efreicraft.ludos.core.games.interfaces.Game;
+import fr.efreicraft.ludos.core.players.Player;
+import fr.efreicraft.ludos.core.teams.TeamRecord;
+import org.bukkit.Material;
+import org.bukkit.World;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class LudosGame extends Game {
+    @Override
+    public void preMapParse(World world) {
+
+    }
+
+    @Override
+    public void postMapParse() {
+
+    }
+
+    @Override
+    public void setupScoreboard(Player player) {
+
+    }
+
+    @Override
+    public EnumMap<Material, String> getGamePointsMaterials() {
+        return null;
+    }
+
+    @Override
+    public Map<String, TeamRecord> getTeamRecords() {
+        return null;
+    }
+}
