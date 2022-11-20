b18db52b71a7f7be1cef8181d63a42dcbef02724
DocSystem <42613513+DocSystem@users.noreply.github.com>
2022-11-20 13:53:28 +0100

    Remove unused import

--- games/sumo/src/main/java/fr/efreicraft/ludos/games/sumo/EventListener.java
+++ games/sumo/src/main/java/fr/efreicraft/ludos/games/sumo/EventListener.java
@@ -6,7 +6,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public record EventListener(GameLogic sumo) implements Listener {
 
