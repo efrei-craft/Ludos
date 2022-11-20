41a29278d548426f32511753c1cb59facf482150
Niilyx _ <nait.meddour.idir100@gmail.com>
2022-11-21 00:39:32 +0100

    boilerplate

---
+++ games/rush/src/main/java/fr/efreicraft/ludos/games/rush/GameLogic.java
@@ -0,0 +1,25 @@
+package fr.efreicraft.ludos.games.rush;
+
+import fr.efreicraft.ludos.core.teams.Team;
+
+public class GameLogic {
+
+    //TODO
+    /**
+     * Vérifie si le lit de *team* a été détruit.
+     * @param team L'équipe à vérifier
+     * @return {@code true} si le lit de team est détruit, sinon {@code false}.
+     */
+    public boolean bedDestroyed(Team team) {
+        return false;
+    }
+
+    /**
+     * Déclare le lit de *team* comme étant détruit.
+     * @param team L'équipe qui a perdu son lit
+     */
+    public void bedDestroyed(Team team, boolean destroyed) {
+
+    }
+
+}
