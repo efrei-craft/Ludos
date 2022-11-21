15f14956d7e282f7c1f17d0cf6e63493add79062
Niilyx <nait.meddour.idir100@gmail.com>
2022-11-21 12:28:29 +0100

    avancement :trollface:

--- games/rush/src/main/java/fr/efreicraft/ludos/games/rush/EventListener.java
+++ games/rush/src/main/java/fr/efreicraft/ludos/games/rush/EventListener.java
@@ -1,7 +1,14 @@
 package fr.efreicraft.ludos.games.rush;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 
 public record EventListener(GameLogic logic) implements Listener {
+
+    @EventHandler
+    public void onBreakBlock(BlockBreakEvent event) {
+        
+    }
 
 }
