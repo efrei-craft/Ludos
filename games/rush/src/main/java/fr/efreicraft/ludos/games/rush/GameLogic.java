aee830e40ada2701d4bc68fb7f5c4332c1b3e6a5
Niilyx <nait.meddour.idir100@gmail.com>
2022-11-23 19:26:39 +0100

    avancement

--- games/rush/src/main/java/fr/efreicraft/ludos/games/rush/GameLogic.java
+++ games/rush/src/main/java/fr/efreicraft/ludos/games/rush/GameLogic.java
@@ -2,26 +2,67 @@ package fr.efreicraft.ludos.games.rush;
 
 import fr.efreicraft.ludos.core.Core;
 import fr.efreicraft.ludos.core.teams.Team;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Villager;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Merchant;
+import org.bukkit.inventory.MerchantRecipe;
 import org.bukkit.scheduler.BukkitTask;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class GameLogic {
 
+    private World world;
+
     private int yDeath;
-    final Villager merchant;
+    Merchant merchant;
 
     private BukkitTask stopWatchTask;
-    int time;
+
+    /**
+     * Le temps écoulé depuis le début de la partie. Techniquement, rien
+     * n'arrête l'incrémentation de cette variable, sauf la fin de partie,
+     * mais bon on aura pas d'overflow après
+     */
+    int time = 0;
 
     private Set<Team> bedDestroyed = new HashSet<>(4);
 
     public GameLogic() {
-        merchant = null;
+    }
+
+    public void world(World world) {
+        this.world = world;
+        setMerchant();
+    }
+
+    private void setMerchant() {
+        Merchant merchant = Bukkit.createMerchant(Component.text("Boutique").color(NamedTextColor.YELLOW).decorate(TextDecoration.BOLD));
+        merchant.setRecipes(getTrades());
+
+        this.merchant = merchant;
+    }
+
+    private List<MerchantRecipe> getTrades() {
+        List<MerchantRecipe> recipeList = new ArrayList<>();
+
+        MerchantRecipe blocs = new MerchantRecipe(new ItemStack(Material.SANDSTONE, 4), 0, 0, false, 0, 1, 0, 0, true);
+        recipeList.add(blocs);
+
+        MerchantRecipe sword1 = new MerchantRecipe(new ItemStack(Material.STONE_SWORD), 0, 0, false, 0, 1, 0, 0, true);
+        recipeList.add(sword1);
+
+        MerchantRecipe pick1 = new MerchantRecipe(new ItemStack(Material.STONE_PICKAXE), 0, 0, false, 0, 1, 0, 0, true);
+        recipeList.add(pick1);
+
+        return recipeList;
     }
 
     public void yDeath(int yDeath) {
@@ -30,8 +71,18 @@ public class GameLogic {
 
     public void startStopwatch() {
         this.stopWatchTask = Bukkit.getScheduler().runTaskTimer(Core.get().getGameManager().getCurrentPlugin(), () -> {
+            if (time % 5 == 0) {
+
+            }
+
+
             time++;
         }, 0, 20);
+    }
+
+    public void stopStopwatch() {
+        if (this.stopWatchTask != null)
+            this.stopWatchTask.cancel();
     }
 
     /**
