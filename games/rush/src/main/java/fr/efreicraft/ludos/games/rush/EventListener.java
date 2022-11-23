311b469a56131bf83a5490fa9f32be744a6acaa7
Niilyx _ <nait.meddour.idir100@gmail.com>
2022-11-23 13:03:02 +0100

    handle le kill direct de joueurs

--- games/rush/src/main/java/fr/efreicraft/ludos/games/rush/EventListener.java
+++ games/rush/src/main/java/fr/efreicraft/ludos/games/rush/EventListener.java
@@ -1,9 +1,12 @@
 package fr.efreicraft.ludos.games.rush;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public record EventListener(GameLogic logic) implements Listener {
 
@@ -11,6 +14,18 @@ public record EventListener(GameLogic logic) implements Listener {
     public void onBreakBlock(BlockBreakEvent event) {
         if (event.getBlock().getType() == Material.OBSIDIAN) {
             event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onKill(EntityDamageEvent event) {
+        if (event instanceof EntityDamageByEntityEvent) {
+            Player victim = (Player) event.getEntity();
+
+            // SI le coup est fatal...
+            if (victim.getHealth() - event.getFinalDamage() <= 0) {
+                logic.handleFinishOffByPlayer((Player) ((EntityDamageByEntityEvent) event).getDamager());
+            }
         }
     }
 
--- games/rush/src/main/java/fr/efreicraft/ludos/games/rush/GameLogic.java
+++ games/rush/src/main/java/fr/efreicraft/ludos/games/rush/GameLogic.java
@@ -1,12 +1,24 @@
 package fr.efreicraft.ludos.games.rush;
 
+import fr.efreicraft.ludos.core.Core;
 import fr.efreicraft.ludos.core.teams.Team;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class GameLogic {
 
     private int yDeath;
     final Villager merchant;
+
+    private BukkitTask stopWatchTask;
+    int time;
+
+    private Set<Team> bedDestroyed = new HashSet<>(4);
 
     public GameLogic() {
         merchant = null;
@@ -15,7 +27,21 @@ public class GameLogic {
     public void yDeath(int yDeath) {
         this.yDeath = yDeath;
     }
-    
+
+    public void startStopwatch() {
+        this.stopWatchTask = Bukkit.getScheduler().runTaskTimer(Core.get().getGameManager().getCurrentPlugin(), () -> {
+            time++;
+        }, 0, 20);
+    }
+
+    /**
+     * Donne les récompenses à la team du joueur ayant donné le coup de grâce au mort
+     * @param killer Le tueur
+     */
+    public void handleFinishOffByPlayer(Player killer) {
+
+    }
+
     //TODO
     /**
      * Vérifie si le lit de *team* a été détruit.
@@ -23,7 +49,7 @@ public class GameLogic {
      * @return {@code true} si le lit de team est détruit, sinon {@code false}.
      */
     public boolean bedDestroyed(Team team) {
-        return false;
+        return bedDestroyed.contains(team);
     }
 
     /**
@@ -31,7 +57,10 @@ public class GameLogic {
      * @param team L'équipe qui a perdu son lit
      */
     public void bedDestroyed(Team team, boolean destroyed) {
-
+        if (destroyed)
+            bedDestroyed.add(team);
+        else
+            bedDestroyed.remove(team);
     }
 
 }
