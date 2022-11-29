8efc359dd304f0f88895e374e62b4e3c62621fed
Niilyx _ <nait.meddour.idir100@gmail.com>
2022-11-29 01:27:54 +0100

    préparation de placement de lit

--- games/rush/src/main/java/fr/efreicraft/ludos/games/rush/Utils.java
+++ games/rush/src/main/java/fr/efreicraft/ludos/games/rush/Utils.java
@@ -1,8 +1,12 @@
 package fr.efreicraft.ludos.games.rush;
 
 import fr.efreicraft.ludos.core.Core;
+import org.bukkit.Location;
+import org.bukkit.block.BlockFace;
 
 import javax.annotation.Nullable;
+
+import static org.bukkit.block.BlockFace.*;
 
 public class Utils {
 
@@ -13,5 +17,31 @@ public class Utils {
 
     public static fr.efreicraft.ludos.core.teams.Team getTeam(String team) {
         return Core.get().getTeamManager().getTeam(team);
+    }
+
+    /**
+     * ...
+     * @param reference Le point de référence selon lequel mesurer
+     * @param self Le point sujet à la mesure
+     * @return Une des <b>quatres</b> directions cardinales OU {@link BlockFace#SELF}
+     */
+    //TODO tester ça là
+    public static BlockFace whereAmIRelatedTo(Location reference, Location self) {
+        int x = reference.getBlockX() - self.getBlockX();
+        int z = reference.getBlockZ() - self.getBlockZ();
+
+        if (x == 0) {
+            if (z > 0)
+                return SOUTH;
+            else if (z < 0)
+                return NORTH;
+            else
+                return SELF;
+        } else {
+            if (x > 0)
+                return EAST;
+            else
+                return WEST;
+        }
     }
 }
