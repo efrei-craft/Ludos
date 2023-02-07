bf93b13b06f5c4e289ef132f5541abbdd59f0a47
Niilyx _ <nait.meddour.idir100@gmail.com>
2023-02-07 21:26:35 +0100

    fix NullPointerException quand le jeu est reset juste avant le début de la game

--- core/src/main/java/fr/efreicraft/ludos/core/games/runnables/GameCountdown.java
+++ core/src/main/java/fr/efreicraft/ludos/core/games/runnables/GameCountdown.java
@@ -5,6 +5,7 @@ import fr.efreicraft.ludos.core.games.GameManager;
 import fr.efreicraft.ludos.core.utils.MessageUtils;
 import fr.efreicraft.ludos.core.utils.SoundUtils;
 import fr.efreicraft.ludos.core.utils.TitleUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -22,6 +23,11 @@ public class GameCountdown extends BukkitRunnable {
 
     @Override
     public void run() {
+        if (Core.get().getGameManager().getCurrentGame() == null) {
+            Bukkit.getLogger().warning("La tache de countdown de début de partie continue à être exécutée malgré qu'on soit pas en jeu. Cela peut arriver si vous faites /game reset pendant ledit countdown. Annulation de la tache...");
+            this.cancel();
+            return;
+        }
         if (countdown == 10) {
             SoundUtils.broadcastSound(Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
         } else if (countdown == 5 || countdown == 4 || countdown == 3 || countdown == 2 || countdown == 1) {
