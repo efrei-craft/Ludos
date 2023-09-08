b6fcc8d9272da91e20c05ecedc112ad9bc3d0027
Niilyx _ <nait.meddour.idir100@gmail.com>
2023-09-08 23:35:20 +0200

    Corrections spleef

--- games/spleef/src/main/java/fr/efreicraft/ludos/games/spleef/EventListener.java
+++ games/spleef/src/main/java/fr/efreicraft/ludos/games/spleef/EventListener.java
@@ -1,8 +1,5 @@
 package fr.efreicraft.ludos.games.spleef;
 
-import fr.efreicraft.ludos.core.Core;
-import fr.efreicraft.ludos.core.maps.points.SpawnPoint;
-import io.papermc.paper.entity.LookAnchor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -11,18 +8,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.inventory.InventoryInteractEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.Vector;
-
-import java.util.ArrayList;
-import java.util.Random;
 
 public record EventListener(GameLogic logic) implements Listener {
 
@@ -32,7 +25,10 @@ public record EventListener(GameLogic logic) implements Listener {
         if (player.isDead()) return;
 
         switch (event.getCause()) {
-            case FALL, SUFFOCATION -> event.setCancelled(true);
+            case FALL, SUFFOCATION -> {
+                event.setCancelled(true);
+                event.setDamage(0);
+            }
             case ENTITY_EXPLOSION, BLOCK_EXPLOSION -> event.setDamage(0);
             case LAVA, VOID -> {
                 event.setCancelled(true);
@@ -63,7 +59,7 @@ public record EventListener(GameLogic logic) implements Listener {
 
     @EventHandler
     public void onSnowballTouch(ProjectileHitEvent event) {
-        if (event.getHitEntity() != null) event.setCancelled(true);
+//        if (event.getHitEntity() != null) event.setCancelled(true);
     }
 
     @EventHandler
@@ -82,11 +78,16 @@ public record EventListener(GameLogic logic) implements Listener {
     @EventHandler
     public void onSnowballConsume(ProjectileLaunchEvent event) {
         Player player = (Player) event.getEntity().getShooter();
-        player.getInventory().setItem(1, new ItemStack(Material.SNOWBALL, 8));
+        player.getInventory().setItem(1, new ItemStack(Material.SNOWBALL, 16));
     }
 
     @EventHandler
     public void onDrop(PlayerDropItemEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onHit(EntityDamageByEntityEvent event) {
         event.setCancelled(true);
     }
 }
