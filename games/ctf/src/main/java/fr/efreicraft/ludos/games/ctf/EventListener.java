d55bc1164c653c5816e8c1c3af3e9c11269ad2e7
orwenn <46846090+orwenn22@users.noreply.github.com>
2023-09-13 12:11:30 +0200

    CTF : amélioration code

--- games/ctf/src/main/java/fr/efreicraft/ludos/games/ctf/EventListener.java
+++ games/ctf/src/main/java/fr/efreicraft/ludos/games/ctf/EventListener.java
@@ -2,7 +2,6 @@ package fr.efreicraft.ludos.games.ctf;
 
 import fr.efreicraft.ludos.core.Core;
 import fr.efreicraft.ludos.core.players.LudosPlayer;
-import fr.efreicraft.ludos.core.utils.MessageUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -16,36 +15,22 @@ public record EventListener(GameLogic ctfLogic) implements Listener {
 
     @EventHandler
     public void onPlayerBreakBlock(BlockBreakEvent event) {
-        LudosPlayer ludos_player = Core.get().getPlayerManager().getPlayer(event.getPlayer());
+        LudosPlayer ludosPlayer = Core.get().getPlayerManager().getPlayer(event.getPlayer());
 
-        String team_to_check = "";  //on doit vérifier si le joueur n'appartient pas à cette team avant de casser le drapeau
+        String teamToCheck;  //on doit vérifier si le joueur n'appartient pas à cette team avant de casser le drapeau
 
+        //Obtenir la team du drapeau
         switch (event.getBlock().getBlockData().getMaterial()) {
-            case RED_BANNER -> team_to_check = "RED";
-            case BLUE_BANNER -> team_to_check = "BLUE";
+            case RED_BANNER -> teamToCheck = "RED";
+            case BLUE_BANNER -> teamToCheck = "BLUE";
             default -> {
                 event.setCancelled(true);
                 return;
             }
         }
 
-        //Vérifier si le joueur n'essaie pas de casser le drapeau de sa propre équipe
-        if(Core.get().getTeamManager().getTeam(team_to_check).getPlayers().contains(ludos_player)) {
-            ludos_player.sendMessage(
-                    MessageUtils.ChatPrefix.GAME,
-                    "Tu ne peux pas récupérer ton propre drapeau !"
-            );
-            event.setCancelled(true);
-            return;
-        }
-
-        ludos_player.sendMessage(
-                MessageUtils.ChatPrefix.GAME,
-                "Tu as récupéré le drapeau adverse !"
-        );
-
-        //TODO : stocker les joueurs possédants les drapeaux dans GameLogic
-
-
+        //vérifier si un joueur n'essaie pas de casser son propre drapeau,
+        //puis stocker le joueur possédant le drapeau dans gameLogic
+        event.setCancelled(ctfLogic.BreakFlag(ludosPlayer, teamToCheck));
     }
 }
--- games/ctf/src/main/java/fr/efreicraft/ludos/games/ctf/GameLogic.java
+++ games/ctf/src/main/java/fr/efreicraft/ludos/games/ctf/GameLogic.java
@@ -1,24 +1,22 @@
 package fr.efreicraft.ludos.games.ctf;
 
+import fr.efreicraft.ludos.core.Core;
 import fr.efreicraft.ludos.core.players.LudosPlayer;
+import fr.efreicraft.ludos.core.utils.MessageUtils;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class GameLogic {
-    public Location redLocation;
-    public Location blueLocation;
+    private Location redLocation = null;
+    private Location blueLocation = null;
 
-    public int scoreTeamRed;
-    public int scoreTeamBlue;
+    private int scoreTeamRed = 0;
+    private int scoreTeamBlue = 0;
 
     public GameLogic() {
-        redLocation = null;
-        blueLocation = null;
 
-        scoreTeamRed = 0;
-        scoreTeamBlue = 0;
     }
 
     //Faire apparaître les drapeaux sur la map, et stocker leurs positions
@@ -34,5 +32,29 @@ public class GameLogic {
         player.entity().setGameMode(GameMode.SURVIVAL);
         player.entity().getInventory().addItem(new ItemStack(Material.DIAMOND_SWORD));
         //TODO : ajouter + d'équipement ici ?
+    }
+
+
+    //player : joueur qui a cassé le drapeau
+    //teamToCheck : la team du drapeau qui a été cassé (utilisé pour empécher une équipe de détruire son propre drapeau)
+    //retourne true pour annuler l'event du cassage, sinon retourne false
+    public boolean BreakFlag(LudosPlayer player, String teamToCheck) {
+        //Vérifier si le joueur n'essaie pas de casser le drapeau de sa propre équipe
+        if(Core.get().getTeamManager().getTeam(teamToCheck).getPlayers().contains(player)) {
+            player.sendMessage(
+                    MessageUtils.ChatPrefix.GAME,
+                    "Tu ne peux pas récupérer ton propre drapeau !"
+            );
+            return true;
+        }
+
+        player.sendMessage(
+                MessageUtils.ChatPrefix.GAME,
+                "Tu as récupéré le drapeau adverse !"
+        );
+
+        //TODO : stocker les joueurs possédant les drapeaux dans la classe
+
+        return false;
     }
 }
