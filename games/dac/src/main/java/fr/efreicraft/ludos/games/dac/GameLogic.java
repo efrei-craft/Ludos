6ed415f43e1387c3dd244304491d4d0559ad79b9
nat.io <natemps.io@gmail.com>
2023-12-09 19:40:40 +0100

    Amélioration du code

--- games/dac/src/main/java/fr/efreicraft/ludos/games/dac/GameLogic.java
+++ games/dac/src/main/java/fr/efreicraft/ludos/games/dac/GameLogic.java
@@ -1,39 +1,22 @@
 package fr.efreicraft.ludos.games.dac;
-
 import fr.efreicraft.ludos.core.Core;
-import fr.efreicraft.ludos.core.maps.points.SpawnPoint;
 import fr.efreicraft.ludos.core.players.LudosPlayer;
-import fr.efreicraft.ludos.core.teams.Team;
-import fr.efreicraft.ludos.core.utils.ActionBarUtils;
 import fr.efreicraft.ludos.core.utils.MessageUtils;
 import fr.efreicraft.ludos.core.utils.TitleUtils;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.PluginCommand;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-
 import java.util.ArrayList;
-
+import java.util.List;
 public class GameLogic {
+    private List<LudosPlayer> players = new ArrayList<>();
     private Location bassinPosition;
     private Location plateformePosition;
-
-    private Location spawnPosition ;
-
+    private Location spawnPosition;
     private Location boundary1Position;
-
     private Location boundary2Position;
-
     private int round = 1;
-
     private int numberOfPlayers;
-
-    private ArrayList<LudosPlayer> players = new ArrayList<>();
-
-
-
+    private double limitOfPlayers;
     public void setBassinLocation(Location bassinLocation) {
         this.bassinPosition = bassinLocation;
     }
@@ -45,84 +28,83 @@ public class GameLogic {
     public void setNumberOfPlayers(int numberOfPlayers) {
         this.numberOfPlayers = numberOfPlayers;
     }
+    public void setLimitOfPlayers(int numberOfPlayers) {this.limitOfPlayers = Math.ceil(numberOfPlayers/2);}
     public void setPlateformeBoundaries(Location boundary1Location, Location boundary2Location) {
         this.boundary1Position = boundary1Location;
         this.boundary2Position = boundary2Location;
     }
-
     public void setSpawnPosition(Location spawnLocation) {
         this.spawnPosition = spawnLocation;
     }
     public boolean isInBassin(Location position) {
-        return bassinPosition.getBlockY() > position.getY() & bassinPosition.getBlockY()-2<position.getY() & bassinPosition.getBlockX() +4> position.getX() & bassinPosition.getBlockX() -4< position.getX() & bassinPosition.getBlockZ() +4> position.getZ() & bassinPosition.getBlockZ() -4< position.getZ();
+        return bassinPosition.getBlockY() > position.getY() & bassinPosition.getBlockY() - 2 < position.getY()
+                & bassinPosition.getBlockX() + 4 > position.getX() & bassinPosition.getBlockX() - 4 < position.getX()
+                & bassinPosition.getBlockZ() + 4 > position.getZ() & bassinPosition.getBlockZ() - 4 < position.getZ();
     }
-
-    public boolean isOnGround(double positionY){return bassinPosition.getBlockY()==positionY;}
-
+    public boolean isOnGround(double positionY) {
+        return bassinPosition.getBlockY() == positionY;
+    }
     public void onPlayerInBassin(LudosPlayer player) {
         player.sendMessage(MessageUtils.ChatPrefix.GAME, "Tombé dans l'eau");
-        Core.get().getMapManager().getCurrentMap().getWorld().setBlockData(new Location(Core.get().getMapManager().getCurrentMap().getWorld(),player.entity().getLocation().getBlockX(),bassinPosition.getY()-1,player.entity().getLocation().getBlockZ()), Material.BLACK_CONCRETE.createBlockData());
+        Core.get().getMapManager().getCurrentMap().getWorld().setBlockData(new Location(Core.get().getMapManager().getCurrentMap().getWorld(),
+                player.entity().getLocation().getBlockX(),
+                        bassinPosition.getY() - 1,
+                        player.entity().getLocation().getBlockZ()),
+                Material.BLACK_CONCRETE.createBlockData());
         player.entity().teleport(this.spawnPosition);
         nextPlayer(player);
-
     }
-
-    public void movePlateforme(int round){
-        if (!(round==1)){
-        int decalagePlateforme=30*(round-1);
-        for (int y=this.boundary2Position.getBlockY();y>=this.boundary1Position.getBlockY();y--) {
-            for (int z = this.boundary2Position.getBlockZ(); z <= this.boundary1Position.getBlockZ(); z++) {
-                for (int x = this.boundary2Position.getBlockX(); x <= this.boundary1Position.getBlockX(); x++) {
-                    Core.get().getMapManager().getCurrentMap().getWorld().getBlockAt(x, y+decalagePlateforme, z).setType(Core.get().getMapManager().getCurrentMap().getWorld().getBlockAt(x, y, z).getType());
-                    Core.get().getMapManager().getCurrentMap().getWorld().getBlockAt(x, y, z).setType(Material.AIR);
+    public void movePlateforme(int round) {
+        if (round != 1) {
+            int decalagePlateforme = 30 * (round - 1);
+            for (int y = this.boundary2Position.getBlockY(); y >= this.boundary1Position.getBlockY(); y--) {
+                for (int z = this.boundary2Position.getBlockZ(); z <= this.boundary1Position.getBlockZ(); z++) {
+                    for (int x = this.boundary2Position.getBlockX(); x <= this.boundary1Position.getBlockX(); x++) {
+                        Core.get().getMapManager().getCurrentMap().getWorld()
+                                .getBlockAt(x, y + decalagePlateforme, z).setType(Core.get().getMapManager().getCurrentMap().getWorld().getBlockAt(x, y, z).getType());
+                        Core.get().getMapManager().getCurrentMap().getWorld().getBlockAt(x, y, z).setType(Material.AIR);
+                    }
                 }
-            }}
-        this.plateformePosition.setY(this.plateformePosition.getY()+decalagePlateforme);
+            }
+            this.plateformePosition.setY(this.plateformePosition.getY() + decalagePlateforme);
         }
     }
-
     public void onPlayerTouchingGround(LudosPlayer player) {
         player.sendMessage(MessageUtils.ChatPrefix.GAME, "Vous êtes tombé sur une bloc ! Vous êtes éliminé !");
         player.setTeam(Core.get().getTeamManager().getTeam("SPECTATORS"));
         player.entity().setGameMode(org.bukkit.GameMode.SPECTATOR);
         players.remove(player);
         nextPlayer(player);
-
     }
-
     public void nextPlayer(LudosPlayer player) {
-        player.sendMessage(MessageUtils.ChatPrefix.GAME, "Il reste "+players.size()+" joueurs");
-        if (players.size()==1){
-            TitleUtils.broadcastTitle(Core.get().getGameManager().getCurrentGame().getMetadata().color() + "Victoire de "+players.get(0).entity().getName(), "", 0, 2, 0.5f);
-
-        }
-        else if (players.size()<Math.ceil(numberOfPlayers/round)+Math.abs(round%2-1)){
+        player.sendMessage(MessageUtils.ChatPrefix.GAME, "Il reste " + players.size() + " joueurs");
+        if (players.size()==1) {
+            TitleUtils.broadcastTitle("&9" + "Victoire de " + players.get(0).entity().getName(),
+                    "", 0, 2, 0.5f);
+        } else if (players.size() <limitOfPlayers + Math.abs(round % 2 - 1)) {
             this.round++;
             playRound(this.round);
-        }
-        else{
-        for (LudosPlayer currentPlayer : players) {
-            if (currentPlayer == player && players.lastIndexOf(player)<players.size()) {
-                players.iterator().next().entity().teleport(this.plateformePosition);
+        } else {
+            for (LudosPlayer currentPlayer : players) {
+                if (currentPlayer == player && players.lastIndexOf(player) < players.size()) {
+                    players.iterator().next().entity().teleport(this.plateformePosition);
+                } else {
+                    players.get(0).entity().teleport(this.plateformePosition);
+                }
             }
-            else{
-                players.get(0).entity().teleport(this.plateformePosition);
-            }
-
         }
-    }}
-
-    public void setPlayingPlayers(){
+    }
+    public void setPlayingPlayers() {
         this.players.addAll(Core.get().getTeamManager().getTeam("PLAYERS").getPlayers());
     }
-    public void playRound(int round){
-        TitleUtils.broadcastTitle(Core.get().getGameManager().getCurrentGame().getMetadata().color() + "Round "+round, "", 0, 2, 0.5f);
+    public void playRound(int round) {
+        TitleUtils.broadcastTitle(Core.get().getGameManager().getCurrentGame().getMetadata().color() + "Round " + round, "", 0, 2, 0.5f);
         movePlateforme(round);
         players.get(0).entity().teleport(this.plateformePosition);
     }
-
     public void onGameStart() {
         setNumberOfPlayers(Core.get().getTeamManager().getTeam("PLAYERS").getPlayers().size());
+        setLimitOfPlayers(numberOfPlayers);
         setPlayingPlayers();
         playRound(1);
     }
--- games/dac/src/main/java/fr/efreicraft/ludos/games/dac/LudosGame.java
+++ games/dac/src/main/java/fr/efreicraft/ludos/games/dac/LudosGame.java
@@ -1,5 +1,4 @@
 package fr.efreicraft.ludos.games.dac;
-
 import com.google.common.collect.ImmutableMap;
 import fr.efreicraft.ludos.core.Core;
 import fr.efreicraft.ludos.core.games.annotations.GameMetadata;
@@ -10,47 +9,36 @@ import fr.efreicraft.ludos.core.teams.TeamRecord;
 import fr.efreicraft.ludos.core.utils.ColorUtils;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.*;
-import org.bukkit.entity.Player;
-
 import java.util.EnumMap;
 import java.util.Map;
-
 import static fr.efreicraft.ludos.core.teams.DefaultTeamRecordBuilder.DefaultTeamRecords.ONLY_SPECTATOR;
-
 @GameMetadata(
         name = "Dé à coudre",
         description = "Essayer de tomber dans l'eau et passez les trois rounds pour gagner !",
         authors = {"Nat.io"},
-        color = "&b",
+        color = "&9",
         mapFolder = "DAC",
-
         rules = @GameRules(
                 maxPlayers = 8
         )
 )
 public class LudosGame extends Game {
-
     private final GameLogic gameLogic;
-
     public LudosGame() {
         super();
         this.gameLogic = new GameLogic();
         this.setEventListener(new EventListener(this.gameLogic));
     }
-
     @Override
     public void preMapParse(World world) {
         // Nothing to do here
     }
-
     @Override
     public void beginGame() {
         super.beginGame();
         Core.get().getTeamManager().getTeam("PLAYERS").setFriendlyFire(false);
         gameLogic.onGameStart();
-
     }
-
     @Override
     public void postMapParse() {
         this.gameLogic.setBassinLocation(
@@ -66,25 +54,19 @@ public class LudosGame extends Game {
         this.gameLogic.setSpawnPosition(
                 Core.get().getMapManager().getCurrentMap().getSpawnPoints().get(Core.get().getTeamManager().getTeam("PLAYERS")).get(5).getLocation()
         );
-
-
-
     }
-
     @Override
     public void setupScoreboard(LudosPlayer player) {
         // No scoreboard for now
     }
-
     @Override
     public EnumMap<Material, String> getGamePointsMaterials() {
         EnumMap<Material, String> gamePointsMaterials = new EnumMap<>(Material.class);
         gamePointsMaterials.put(Material.PRISMARINE_BRICKS, "BASSIN");
         gamePointsMaterials.put(Material.END_STONE_BRICKS, "BORDER_PLATEFORME");
-        gamePointsMaterials.put(Material.CHISELED_STONE_BRICKS,"SPAWN_PLATEFORME");
+        gamePointsMaterials.put(Material.CHISELED_STONE_BRICKS, "SPAWN_PLATEFORME");
         return gamePointsMaterials;
     }
-
     @Override
     public Map<String, TeamRecord> getTeamRecords() {
         return ImmutableMap.<String, TeamRecord>builder()
@@ -104,7 +86,6 @@ public class LudosGame extends Game {
                             p.entity().getInventory().clear();
                             p.entity().getInventory().setArmorContents(null);
                             p.entity().setGameMode(GameMode.ADVENTURE);
-
                         }
                 ))
                 .putAll(ONLY_SPECTATOR.getTeamRecords())
